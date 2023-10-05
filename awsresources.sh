75fbea150aff67f341b402e2edb7a83b76c69059
anjaneyulu77 <112678403+anjaneyulu77@users.noreply.github.com>
2023-10-05 19:30:17 +0530

    aws ec2 updated

--- awsresources.sh
+++ awsresources.sh
@@ -3,7 +3,7 @@
 set -x
 echo "this is list of buckets"
 aws s3 ls
-
+echo "describes ec2 instances"
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
 
 aws lambda list-functions
